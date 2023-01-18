2a1a63c74a5b0b3598e7587b49d1de034e80a1b1
Ravenspeaks <90291456+Ravenspeaks@users.noreply.github.com>
2023-01-18 15:20:02 -0800

    updated .Js with get DOM getElementId

--- docs/app.js
+++ docs/app.js
@@ -1,9 +1,26 @@
 "use strict";
 
-function selectPlan( planName, monthlyRate ) {
+function calculateAnnualRate( monthlyRate) {
     let annualRate = monthlyRate * 12;
+    return annualRate;
+}
+
+function selectPlan( planName, monthlyRate ) {
+    let annualRate = calculateAnnualRate( monthlyRate );
     console.log( planName + " plan Selected! ");
     console.log( '$'+ monthlyRate + '/mo' );
     console.log( '$'+ annualRate + '/yr' );
+
+    let planNameElement = document.getElementById( "planname" );
+    planNameElement.innerHTML = planName;
+
+    let monthlyRateElement = document.getElementById( "monthlyrate" );
+    monthlyRateElement.innerHTML = '$' + monthlyRate + '/mo';
+
+    let annualRateElement = document.getElementById( "annualrate" );
+    annualRateElement.innerHTML = '$' + annualRate + '/yr';
+
+    let today = new Date();
 }
+
 
