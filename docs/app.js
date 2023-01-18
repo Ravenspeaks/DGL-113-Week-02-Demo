985e85b88972fd213b7895feea982fde243d3e1e
Ravenspeaks <90291456+Ravenspeaks@users.noreply.github.com>
2023-01-18 14:39:30 -0800

    Selected Plan functions JS and Html

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,9 @@
 "use strict";
 
-function selectPlan( planName ) {
-    console.log( planName + "plan Selected!");
+function selectPlan( planName, monthlyRate ) {
+    let annualRate = monthlyRate * 12;
+    console.log( planName + " plan Selected! ");
+    console.log( '$'+ monthlyRate + '/mo' );
+    console.log( '$'+ annualRate + '/yr' );
 }
 
